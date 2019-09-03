2d787f46272485183feabb6b151245a86ada411b
alxv54 <47007441+alxv54@users.noreply.github.com>
2019-09-03 11:31:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,19 +4,15 @@
 import Capacity
 
 
-t = CapacitySet.CapacitySet({1,2,3})
-
-
-t.mu=
-
-t.mu= {frozenset(): 0, frozenset({'3', '2', '4', '1'}): 1, frozenset({'1'}): 0.1, frozenset({'2'}): 0.2, 
+t = CapacitySet.CapacitySet({frozenset(): 0, frozenset({'3', '2', '4', '1'}): 1, frozenset({'1'}): 0.1, frozenset({'2'}): 0.2, 
        frozenset({'3'}): 0.1, frozenset({'4'}): 0.3, frozenset({'3', '2'}): 0.7082259973350473, 
        frozenset({'3', '1'}): 0.7834792399805287, frozenset({'3', '4', '1'}): 0.9536303899863656, 
        frozenset({'3', '4'}): 0.6538741482956895, frozenset({'3', '2', '1'}): 0.8070513547828126, 
        frozenset({'4', '1'}): 0.3, frozenset({'2', '4'}): 0.32535600340934057, 
        frozenset({'3', '2', '4'}): 0.7394800676114206, frozenset({'2', '4', '1'}): 0.5, 
-       frozenset({'2', '1'}): 0.4297823119322727}
+       frozenset({'2', '1'}): 0.4297823119322727})
 
 
-c= Choquet.Choquet(t)
+for i in range(2**t.N):
+       printf(t.subsets)
 
